058235b0de2da21e949287c3cc11f196f738b5c2
unknown <chidozie.nwobilor@gmail.com>
2015-10-28 07:03:43 -0700

    Fully implemented foodnearme api

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url, include
 
-from .views import FoodTrucks
+from .views import FoodTrucks, FoodNearMe
 
 urlpatterns = patterns('',
     # Allow access to the contacts resource collection
     url(r'^foodtrucks/?$', FoodTrucks.as_view()),
+    url(r'^foodnearme/?$', FoodNearMe.as_view()),
 )
--- api/views.py
+++ api/views.py
@@ -3,18 +3,17 @@ from django.core import serializers
 
 from simple_rest import Resource
 import json
-from .models import FoodTruck
 import requests
 
-
-class FoodTrucks(Resource):
+class FoodQuery:
+    # This is the sfgov food truck endpoint
     foodTruckUrl = 'https://data.sfgov.org/resource/6a9r-agq8.json'
-    def get(self, request, **kwargs):
-        query = {'$where': 'within_circle(location,37.78,-122.4, 1000)', 'status' : 'APPROVED', '$limit' : '5', '$offset' : '0'}      
-        r = requests.get(self.createFoodTruckQuery(query))
-        return HttpResponse(r.json(), content_type='application/json', status=200)
     
-    def createFoodTruckQuery(self, query):
+    # This creates the url and adds the query string using the end point
+    # I ran into a problem with requests ability to encode the query
+    # so I implemented this myself.  I'm using join instead of '+'
+    # because it's the most efficient way to concatenate strings
+    def createFoodTruckUrl(self, query):
         queryList = [self.foodTruckUrl, '?']
         s = ''
         for queryKey in query.keys():
@@ -24,3 +23,37 @@ class FoodTrucks(Resource):
             queryList.append('&')
         queryList.pop()
         return ''.join( queryList )
+    
+    def milesToMeters(miles):
+        return miles * 1609.344
+    
+    def ProcessFoodItems(self, foodTruckList):
+        foodItemDict = {}
+        for foodTruck in foodTruckList:
+            fooditemlist = foodTruck['fooditems'].split(': ')
+            for fooditem in fooditemlist:
+                if fooditem in foodItemDict.keys():
+                    foodItemDict[fooditem].append(foodTruck['objectid'])
+                else:
+                    foodItemDict[fooditem] = [foodTruck['objectid']]
+        return foodItemDict
+
+class FoodTrucks(Resource, FoodQuery):
+    
+    def get(self, request, **kwargs):
+        query = {'$where': 'within_circle(location,37.78,-122.4, 1000)', 'status' : 'APPROVED', '$limit' : '5', '$offset' : '0'}      
+        r = requests.get(self.createFoodTruckUrl(query))
+        return HttpResponse(r.json(), content_type='application/json', status=200)
+    
+    
+class FoodNearMe(Resource, FoodQuery):
+
+    def get(self, request, **kwargs):
+        lat = request.GET.get('lat')
+        long = request.GET.get('long')
+        query = {'$where': 'within_circle(location, ' + lat + ', ' + long + ', 1000)', 'status' : 'APPROVED', '$limit' : '5', '$offset' : '0'}    
+        r = requests.get(self.createFoodTruckUrl(query))
+        fooditemDict = self.ProcessFoodItems(r.json())
+        result = json.dumps(fooditemDict)
+        return HttpResponse(result, content_type='application/json', status=200)
+        
