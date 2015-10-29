aa29bd13cef85c3f7aa8c18fc0757a7fca062f92
dagclo <chidozie.nwobilor@gmail.com>
2015-10-29 07:38:35 -0700

    removing print statement

--- api/views.py
+++ api/views.py
@@ -52,7 +52,7 @@ class FoodNearMe(Resource, FoodQuery):
     # Required Parameters: lat and lng for latitude and longitude.  
     # returns bad request if either of those are missing
     # Optional Parameters: meters.  This is set to 1000 if missing.  If less than 0, raises a bad error
-    # Returns: Dictionary of fooditems => food truck objectids
+        # Returns: Dictionary of fooditems => food truck objectids
     def get(self, request, **kwargs):
         lat = request.GET.get('lat')
         long = request.GET.get('lng')
@@ -60,15 +60,11 @@ class FoodNearMe(Resource, FoodQuery):
         if meters == None:
             meters = 1000
         if meters < 0:
-           return HttpResponseBadRequest('<h1>meters parameter can not be less than zero</h1>') 
-        if lat == None:
-            return HttpResponseBadRequest('<h1>Parameter lat is missing</h1>')
-        elif long == None:
-            return HttpResponseBadRequest('<h1>Parameters lng are missing</h1>')        
+           return HttpResponseBadRequest('<h1>meters parameter can't be less than zero</h1>') 
+        if lat == None || long == None:
+            return HttpResponseBadRequest('<h1>Parameters lng or lat are missing</h1>')
         query = {'$where': 'within_circle(location, ' + lat + ', ' + long + ', ' + meters + ')', 'status' : 'APPROVED', '$limit' : '5', '$offset' : '0'}    
         r = requests.get(self.createFoodTruckUrl(query))
-        fooditemDict = self.ProcessFoodItems(r.json())
-        print fooditemDict
         result = json.dumps(fooditemDict)
         return HttpResponse(result, content_type='application/json', status=200)
         
